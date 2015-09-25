8fc6ee4661aa627098f68d8a932ad5757bb77ce5
Sergio-HP <sergiohidalgo.94@gmail.com>
2015-09-25 16:27:13 -0300

    Plugins

---
+++ contenido/plugins/wordfence/tmp/configCache.php
@@ -0,0 +1,4 @@
+<?php
+/* Wordfence temporary file security header */
+echo "Nothing to see here!\n"; exit(0);
+?>a:11:{s:9:"cacheType";s:0:"";s:9:"howGetIPs";s:0:"";s:6:"apiKey";s:160:"e3a9b1fa3729757c0cd468af289ce6a882d8aef5c875ada23a1812a398cc6f874177d8ce6bd1a9247720379565c109b0d97d4c4fcc9d6dcf0d27225a7c8574d40b9956836edffe04eac331644b46f9a8";s:20:"loginSec_lockoutMins";s:3:"240";s:15:"firewallEnabled";s:1:"1";s:18:"liveTrafficEnabled";s:1:"1";s:14:"disableCookies";s:1:"0";s:10:"tourClosed";s:0:"";s:17:"actUpdateInterval";s:0:"";s:7:"debugOn";s:1:"0";s:13:"welcomeClosed";s:1:"1";}
