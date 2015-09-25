676f0b9ce61b0f955292c37cba12f0c128554ead
Sergio-HP <sergiohidalgo.94@gmail.com>
2015-09-25 15:53:26 -0300

    Save coding

--- contenido/themes/twentytwelve/functions.php
+++ contenido/themes/twentytwelve/functions.php
@@ -494,3 +494,19 @@ function twentytwelve_customize_preview_js() {
 	wp_enqueue_script( 'twentytwelve-customizer', get_template_directory_uri() . '/js/theme-customizer.js', array( 'customize-preview' ), '20141120', true );
 }
 add_action( 'customize_preview_init', 'twentytwelve_customize_preview_js' );
+
+/*global $current_user;
+get_currentuserinfo();
+
+$email = $current_user->user_email;
+$info_usuario = array('email' => $email);
+
+
+echo 'Username: ' . $current_user->user_login . "\n";
+echo 'User email: ' . $current_user->user_email . "\n";
+echo 'User level: ' . $current_user->user_level . "\n";
+echo 'User first name: ' . $current_user->user_firstname . "\n";
+echo 'User last name: ' . $current_user->user_lastname . "\n";
+echo 'User display name: ' . $current_user->display_name . "\n";
+echo 'User ID: ' . $current_user->ID . "\n";
+*/
